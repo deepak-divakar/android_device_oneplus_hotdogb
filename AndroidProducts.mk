74581a5f7cb5ba71611dbcec256be72bdb1ac473
deepak-divakar <deepak112358@gmail.com>
2020-12-04 06:25:15 +0530

    Bring up maru_hotdogb on Lineage OS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,15 @@
 #
 # Copyright (C) 2019 The LineageOS Project
+# Copyright 2015-2016 Preetam J. D'Souza
+# Copyright 2016 The Maru OS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_hotdogb.mk
+    $(LOCAL_DIR)/lineage_hotdogb.mk \
+    $(LOCAL_DIR)/maru_hotdogb.mk
+
+COMMON_LUNCH_CHOICES := \
+    maru_hotdogb-userdebug \
+    maru_hotdogb-eng
